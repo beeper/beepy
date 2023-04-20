679bec4a00c5838e7314b0924d41455026e59a2d
w4ilun <w4ilun@users.noreply.github.com>
2023-04-20 11:57:47 -0400

    Update setup.sh

--- raspberrypi/setup.sh
+++ raspberrypi/setup.sh
@@ -15,8 +15,9 @@ apt-get -y upgrade < "/dev/null" || { echo "Error: Failed to upgrade apt-get."; 
 apt-get -y install git raspberrypi-kernel-headers < "/dev/null" || { echo "Error: Failed to install dependencies."; exit 1; }
 
 echo "Compiling and installing display driver..."
-git clone https://github.com/w4ilun/Sharp-Memory-LCD-Kernel-Driver.git . || { echo "Error: Failed to clone display driver repository."; exit 1; }
-cd ./Sharp-Memory-LCD-Kernel-Driver
+cd ~/
+git clone https://github.com/w4ilun/Sharp-Memory-LCD-Kernel-Driver.git || { echo "Error: Failed to clone display driver repository."; exit 1; }
+cd ~/Sharp-Memory-LCD-Kernel-Driver
 make || { echo "Error: Failed to compile display driver."; exit 1; }
 make modules_install || { echo "Error: Failed to install display driver."; exit 1; }
 depmod -A || { echo "Error: Failed to update module dependencies."; exit 1; }
@@ -27,8 +28,9 @@ echo -e "framebuffer_width=400\nframebuffer_height=240\ndtoverlay=sharp" > /boot
 sed -i ' 1 s/.*/& fbcon=map:10 fbcon=font:VGA8x8/' /boot/cmdline.txt || { echo "Error: Failed to modify cmdline.txt."; exit 1; }
 
 echo "Compiling and installing keyboard device driver..."
-git clone https://github.com/wallComputer/bbqX0kbd_driver.git . || { echo "Error: Failed to clone keyboard driver repository."; exit 1; }
-cd ./bbqX0kbd_driver
+cd ~/
+git clone https://github.com/wallComputer/bbqX0kbd_driver.git || { echo "Error: Failed to clone keyboard driver repository."; exit 1; }
+cd ~/bbqX0kbd_driver
 ./installer.sh --BBQ20KBD_TRACKPAD_USE BBQ20KBD_TRACKPAD_AS_KEYS --BBQX0KBD_INT BBQX0KBD_USE_INT || { echo "Error: Failed to install keyboard device driver."; exit 1; }
 
 echo "Rebooting..."
