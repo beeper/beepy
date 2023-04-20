a7d29813d7277b6ab34f10b9b66593a3e9573d96
w4ilun <w4ilun@users.noreply.github.com>
2023-04-20 11:17:31 -0400

    Update setup.sh

--- raspberrypi/setup.sh
+++ raspberrypi/setup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 set -e
 
-# Switch to root user
+echo "Switching to root user..."
 sudo -i
 
-# Enable I2C, SPI, and Console Auto login
+echo "Enabling I2C, SPI, and Console Auto login..."
 raspi-config nonint do_i2c 0 || { echo "Error: Failed to enable I2C."; exit 1; }
 raspi-config nonint do_spi 0 || { echo "Error: Failed to enable SPI."; exit 1; }
 raspi-config nonint do_boot_behaviour B2 || { echo "Error: Failed to enable Console Auto login."; exit 1; }
 
-# Install dependencies
+echo "Updating and installing dependencies..."
 apt-get update || { echo "Error: Failed to update apt-get."; exit 1; }
-apt-get install git raspberrypi-kernel-headers || { echo "Error: Failed to install dependencies."; exit 1; }
+apt-get -y install git raspberrypi-kernel-headers < "/dev/null" || { echo "Error: Failed to install dependencies."; exit 1; }
 
-# Compile and install display driver
+echo "Compiling and installing display driver..."
 git clone https://github.com/w4ilun/Sharp-Memory-LCD-Kernel-Driver.git || { echo "Error: Failed to clone display driver repository."; exit 1; }
 cd Sharp-Memory-LCD-Kernel-Driver
 make || { echo "Error: Failed to compile display driver."; exit 1; }
@@ -25,10 +25,10 @@ sudo cp sharp.dtbo /boot/overlays
 echo -e "framebuffer_width=400\nframebuffer_height=240\ndtoverlay=sharp" > /boot/config.txt
 sed -i ' 1 s/.*/& fbcon=map:10 fbcon=font:VGA8x8/' /boot/cmdline.txt || { echo "Error: Failed to modify cmdline.txt."; exit 1; }
 
-#Compile and install keyboard device driver
+echo "Compiling and installing keyboard device driver..."
 git clone https://github.com/wallComputer/bbqX0kbd_driver.git || { echo "Error: Failed to clone keyboard driver repository."; exit 1; }
 cd bbqX0kbd_driver
 ./installer.sh --BBQ20KBD_TRACKPAD_USE BBQ20KBD_TRACKPAD_AS_KEYS --BBQX0KBD_INT BBQX0KBD_USE_INT || { echo "Error: Failed to install keyboard device driver."; exit 1; }
 
-#Reboot
+echo "Rebooting..."
 shutdown -r now || { echo "Error: Failed to reboot."; exit 1; }
