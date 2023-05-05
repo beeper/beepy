2079b8b193b237d47af8d2373acafcce079e1259
Iron Oxidizer <ironoxidizer@gmail.com>
2023-05-05 17:11:33 -0400

    Enable gomuks auto-launch on boot

--- beepberry_os/bcm2708-rpi-zero-w/post-build.sh
+++ beepberry_os/bcm2708-rpi-zero-w/post-build.sh
@@ -3,9 +3,15 @@
 set -u
 set -e
 
-# Add a console on tty1
+# Add an autologin console on tty1
 if [ -e ${TARGET_DIR}/etc/inittab ]; then
     grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
 	sed -i '/GENERIC_SERIAL/a\
-tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
+tty1::respawn:-/bin/sh' ${TARGET_DIR}/etc/inittab
 fi
+
+# Auto launch gomuks on login on tty1
+echo '
+if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
+gomuks -s
+fi' >> ${TARGET_DIR}/etc/profile
--- beepberry_os/bcm2708-rpi-zero-w/post-image.sh
+++ beepberry_os/bcm2708-rpi-zero-w/post-image.sh
@@ -5,6 +5,11 @@ set -e
 GENIMAGE_CFG="../genimage.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
+# Pass an empty rootpath. genimage makes a full copy of the given rootpath to
+# ${GENIMAGE_TMP}/root so passing TARGET_DIR would be a waste of time and disk
+# space. We don't rely on genimage to build the rootfs image, just to insert a
+# pre-built one in the disk image.
+
 trap 'rm -rf "${ROOTPATH_TMP}"' EXIT
 ROOTPATH_TMP="$(mktemp -d)"
 
