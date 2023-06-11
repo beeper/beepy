6f5fbbbee8f2d785092dc2861c418d496b4a4025
SQFMI <github@sqfmi.com>
2023-06-11 16:05:31 -0400

    Update setup.sh

--- raspberrypi/setup.sh
+++ raspberrypi/setup.sh
@@ -24,7 +24,7 @@ sudo sed -i ' 1 s/.*/& fbcon=map:10 fbcon=font:VGA8x16/' /boot/cmdline.txt || { 
 
 echo "Compiling and installing keyboard device driver..."
 cd ~/
-git clone https://github.com/w4ilun/bbqX0kbd_driver.git || { echo "Error: Failed to clone keyboard driver repository."; exit 1; }
+git clone https://github.com/sqfmi/bbqX0kbd_driver.git || { echo "Error: Failed to clone keyboard driver repository."; exit 1; }
 cd ~/bbqX0kbd_driver
 ./installer.sh --BBQ20KBD_TRACKPAD_USE BBQ20KBD_TRACKPAD_AS_KEYS --BBQX0KBD_INT BBQX0KBD_USE_INT || { echo "Error: Failed to install keyboard device driver."; exit 1; }
 
