0354ef7a57cbdba6049aa132b2f52067662b7bcf
drewry pope <drewrypope@gmail.com>
2023-08-27 01:00:13 -0500

    add optimize

---
+++ raspberrypi/optimize.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+curl -s https://raw.githubusercontent.com/dezren39-dev/dezren39/optimize/raspberrypi/optimize-startup.sh | bash
+curl -s https://raw.githubusercontent.com/dezren39-dev/dezren39/optimize/raspberrypi/optimize-power.sh | bash
+curl -s https://raw.githubusercontent.com/dezren39-dev/dezren39/optimize/raspberrypi/optimize-shutdown.sh | bash
