0f3fe4dceaa3385fae1477be65c7dba3f4916756
drewry pope <drewrypope@gmail.com>
2023-08-27 17:25:42 -0500

    set cpu count

--- raspberrypi/optimize.sh
+++ raspberrypi/optimize.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -s "https://raw.githubusercontent.com/dezren39-dev/beepy/optimize/raspberrypi/optimize_boot.sh" | bash
-curl -s "https://raw.githubusercontent.com/dezren39-dev/beepy/optimize/raspberrypi/optimize_motd.sh" | bash
-curl -s "https://raw.githubusercontent.com/dezren39-dev/beepy/optimize/raspberrypi/optimize_power.sh" | bash
-curl -s "https://raw.githubusercontent.com/dezren39-dev/beepy/optimize/raspberrypi/optimize_shutdown.sh" | bash
+curl -s "https://raw.githubusercontent.com/dezren39-dev/beepy/optimize2/raspberrypi/optimize_boot.sh" | bash
+curl -s "https://raw.githubusercontent.com/dezren39-dev/beepy/optimize2/raspberrypi/optimize_motd.sh" | bash
+curl -s "https://raw.githubusercontent.com/dezren39-dev/beepy/optimize2/raspberrypi/optimize_power.sh" | bash
+curl -s "https://raw.githubusercontent.com/dezren39-dev/beepy/optimize2/raspberrypi/optimize_shutdown.sh" | bash
