87c5ac51477801df5478eb3e0049a7ef039709b4
drewry pope <drewrypope@gmail.com>
2023-08-27 01:56:59 -0500

    optimize motd

---
+++ raspberrypi/optimize_motd.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#!/bin/sh
+
+default_motd_sha256="a378977155fb42bb006496321cbe31f74cbda803c3f6ca590f30e76d1afad921"
+local_sha256=$(sha256sum /etc/motd | awk '{print $1}')
+
+if [ "$remote_sha256" = "$local_sha256" ]; then
+  mv /etc/motd /etc/motd.default.backup
+fi
---
+++ raspberrypi/optimize_motd_uname.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+chmod -x /etc/update-motd.d/10-uname
