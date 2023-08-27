09689ca9d75f05f02cb5b8a7f3383a2086c412f4
Drewry Pope <drewrypope@gmail.com>
2023-08-27 03:10:50 -0500

    beeper/beepy/main

--- raspberrypi/optimize.sh
+++ raspberrypi/optimize.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -s "https://raw.githubusercontent.com/dezren39-dev/beepy/optimize/raspberrypi/optimize_boot.sh" | bash
-curl -s "https://raw.githubusercontent.com/dezren39-dev/beepy/optimize/raspberrypi/optimize_motd.sh" | bash
-curl -s "https://raw.githubusercontent.com/dezren39-dev/beepy/optimize/raspberrypi/optimize_power.sh" | bash
-curl -s "https://raw.githubusercontent.com/dezren39-dev/beepy/optimize/raspberrypi/optimize_shutdown.sh" | bash
+curl -s "https://raw.githubusercontent.com/beeper/beepy/main/raspberrypi/optimize_boot.sh" | bash
+curl -s "https://raw.githubusercontent.com/beeper/beepy/main/raspberrypi/optimize_motd.sh" | bash
+curl -s "https://raw.githubusercontent.com/beeper/beepy/main/raspberrypi/optimize_power.sh" | bash
+curl -s "https://raw.githubusercontent.com/beeper/beepy/main/raspberrypi/optimize_shutdown.sh" | bash
