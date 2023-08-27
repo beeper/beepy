e50eedc04f4a8501b77146254a10de0d819667f2
drewry pope <drewrypope@gmail.com>
2023-08-27 01:11:54 -0500

    boot

--- raspberrypi/optimize.sh
+++ raspberrypi/optimize.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-curl -s "https://raw.githubusercontent.com/dezren39-dev/beepy/optimize/raspberrypi/optimize_startup.sh" | bash
+curl -s "https://raw.githubusercontent.com/dezren39-dev/beepy/optimize/raspberrypi/optimize_boot.sh" | bash
 curl -s "https://raw.githubusercontent.com/dezren39-dev/beepy/optimize/raspberrypi/optimize_power.sh" | bash
 curl -s "https://raw.githubusercontent.com/dezren39-dev/beepy/optimize/raspberrypi/optimize_shutdown.sh" | bash
