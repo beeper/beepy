7681010ccb177870b7cfa5e0d9bd75d360b47c56
drewry pope <drewrypope@gmail.com>
2023-08-27 22:33:35 -0500

    escape

--- raspberrypi/disable_login_prompt.sh
+++ raspberrypi/disable_login_prompt.sh
@@ -16,7 +16,7 @@ modify_service_conf() {
   modify_service_conf_new_content=""
   modify_service_conf_found=0
 
-  while IFS= read -r modify_service_conf_line; do
+  while IFS= read -r modify_service_conf_line || [ -n "$modify_service_conf_line" ]; do
     if echo "${modify_service_conf_line}" | grep -q "^${modify_service_conf_linePrefix}"; then
       modify_service_conf_found=1
       case "${modify_service_conf_subcommand}" in
@@ -32,14 +32,13 @@ modify_service_conf() {
           ;;
       esac
     fi
-    modify_service_conf_new_content="${modify_service_conf_new_content}$(printf "%s\n" "${modify_service_conf_line}")"
+    modify_service_conf_new_content=$(printf "%s\n%s" "${modify_service_conf_new_content}" "${modify_service_conf_line}")
   done < "${modify_service_conf_file}"
 
   if [ "${modify_service_conf_found}" -eq 0 ]; then
     exit 1
   fi
 
-  modify_service_conf_new_content=$(printf "%s" "${modify_service_conf_new_content}" | sed -e :a -e '/^\n*$/{$d;N;};/\n$/ba')
   printf "%s\n" "${modify_service_conf_new_content}" > "${modify_service_conf_file}"
 }
 
