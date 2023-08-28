c63bbc626c4059ddb6af8bdfe483b6f993ed2925
drewry pope <drewrypope@gmail.com>
2023-08-28 00:13:52 -0500

    rename

--- raspberrypi/optimize.sh
+++ raspberrypi/optimize.sh
@@ -4,4 +4,4 @@ curl -s "https://raw.githubusercontent.com/beeper/beepy/main/raspberrypi/optimiz
 curl -s "https://raw.githubusercontent.com/beeper/beepy/main/raspberrypi/disable_motd.sh" | bash
 curl -s "https://raw.githubusercontent.com/beeper/beepy/main/raspberrypi/disable_login_prompt.sh" | bash
 curl -s "https://raw.githubusercontent.com/beeper/beepy/main/raspberrypi/disable_cursor.sh" | bash
-curl -s "https://raw.githubusercontent.com/beeper/beepy/main/raspberrypi/optimize_shutdown.sh" | bash
+curl -s "https://raw.githubusercontent.com/beeper/beepy/main/raspberrypi/disable_kernel_logs.sh" | bash
