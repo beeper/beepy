252261d70fa4713d1e6aa6f1bd7d3f093a109549
drewry pope <drewrypope@gmail.com>
2023-08-27 22:51:44 -0500

    escape4

--- raspberrypi/enable_login_prompt.sh
+++ raspberrypi/enable_login_prompt.sh
@@ -6,8 +6,8 @@ modify_service_conf() {
   modify_service_conf_subcommand="$3"
   modify_service_conf_flag="$4"
 
-  # Escape characters that have special meaning in regular expressions
-  modify_service_conf_flag_escaped=$(echo "${modify_service_conf_flag}" | sed -e 's/[]\/$*.^|[]/\\&/g')
+  modify_service_conf_flag_escaped=$(echo "${modify_service_conf_flag}" | sed -e 's/[\\/&]/\\&/g')
+  modify_service_conf_linePrefix_escaped=$(echo "${modify_service_conf_linePrefix}" | sed -e 's/[\\/&]/\\&/g')
 
   if [ ! -f "${modify_service_conf_file}" ]; then
     exit 1
@@ -17,21 +17,23 @@ modify_service_conf() {
   modify_service_conf_found=0
 
   while IFS= read -r modify_service_conf_line || [ -n "$modify_service_conf_line" ]; do
-    if echo "${modify_service_conf_line}" | grep -q -e "^${modify_service_conf_linePrefix}"; then
+    if echo "${modify_service_conf_line}" | grep -q -e "^${modify_service_conf_linePrefix_escaped}"; then
       modify_service_conf_found=1
+      remaining_line=${modify_service_conf_line#${modify_service_conf_linePrefix}}
+      remaining_line=$(echo "${remaining_line}" | awk '{$1=$1};1')
       case "${modify_service_conf_subcommand}" in
         add)
-          if ! echo "${modify_service_conf_line}" | grep -q -e "\\b${modify_service_conf_flag_escaped}\\b"; then
-            remaining_line=$(echo "${modify_service_conf_line#${#modify_service_conf_linePrefix}}" | awk '{$1=$1};1')
+          if ! echo "${remaining_line}" | grep -q -w -e "${modify_service_conf_flag_escaped}"; then
             modify_service_conf_line="${modify_service_conf_linePrefix} ${modify_service_conf_flag} ${remaining_line}"
           fi
           ;;
         del|delete)
-          modify_service_conf_line=$(echo "${modify_service_conf_line}" | sed "s/\\b${modify_service_conf_flag_escaped}\\b//g")
+          remaining_line=$(echo "${remaining_line}" | sed "s/\\b${modify_service_conf_flag_escaped}\\b//g" | awk '{$1=$1};1')
+          modify_service_conf_line="${modify_service_conf_linePrefix}${remaining_line}"
           ;;
       esac
     fi
-    modify_service_conf_new_content=$(printf "%s\n%s" "${modify_service_conf_new_content}" "${modify_service_conf_line}")
+    modify_service_conf_new_content="${modify_service_conf_new_content}${modify_service_conf_line}"$'\n'
   done < "${modify_service_conf_file}"
 
   if [ "${modify_service_conf_found}" -eq 0 ]; then
