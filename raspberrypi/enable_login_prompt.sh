8b98756ef271ca987161ffc0c0864c67215478b9
drewry pope <drewrypope@gmail.com>
2023-08-27 22:38:08 -0500

    escape

--- raspberrypi/enable_login_prompt.sh
+++ raspberrypi/enable_login_prompt.sh
@@ -16,13 +16,13 @@ modify_service_conf() {
   modify_service_conf_new_content=""
   modify_service_conf_found=0
 
-  while IFS= read -r modify_service_conf_line; do
+  while IFS= read -r modify_service_conf_line || [ -n "$modify_service_conf_line" ]; do
     if echo "${modify_service_conf_line}" | grep -q "^${modify_service_conf_linePrefix}"; then
       modify_service_conf_found=1
       case "${modify_service_conf_subcommand}" in
         add)
-          if ! echo "${modify_service_conf_line}" | grep -q "\\b${modify_service_conf_flag_escaped}\\b"; then
-            modify_service_conf_line="${modify_service_conf_line} ${modify_service_conf_flag}"
+          if ! echo "${modify_service_conf_line}" | grep -qw "${modify_service_conf_flag}"; then
+            modify_service_conf_line="${modify_service_conf_linePrefix} ${modify_service_conf_flag} ${modify_service_conf_line#${#modify_service_conf_linePrefix}}"
           fi
           ;;
         del|delete)
@@ -32,15 +32,14 @@ modify_service_conf() {
           ;;
       esac
     fi
-    modify_service_conf_new_content="${modify_service_conf_new_content}$(printf "%s\n" "${modify_service_conf_line}")"
+    modify_service_conf_new_content=$(printf "%s\n%s" "${modify_service_conf_new_content}" "${modify_service_conf_line}")
   done < "${modify_service_conf_file}"
 
   if [ "${modify_service_conf_found}" -eq 0 ]; then
     exit 1
   fi
 
-  modify_service_conf_new_content=$(printf "%s" "${modify_service_conf_new_content}" | sed -e :a -e '/^\n*$/{$d;N;};/\n$/ba')
-  printf "%s\n" "${modify_service_conf_new_content}" > "${modify_service_conf_file}"
+  printf "%s" "${modify_service_conf_new_content}" > "${modify_service_conf_file}"
 }
 
 modify_service_conf "/etc/systemd/system/getty@tty1.service.d/autologin.conf" "ExecStart=-/sbin/agetty" "del" "--skip-login"
