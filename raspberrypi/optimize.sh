d2b2f9c9e7b5207b10ac99651baf71a02bca6076
drewry pope <drewrypope@gmail.com>
2023-08-27 19:22:33 -0500

    optimize

--- raspberrypi/optimize.sh
+++ raspberrypi/optimize.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 curl -s "https://raw.githubusercontent.com/beeper/beepy/main/raspberrypi/optimize_boot.sh" | bash
-curl -s "https://raw.githubusercontent.com/beeper/beepy/main/raspberrypi/optimize_motd.sh" | bash
+curl -s "https://raw.githubusercontent.com/beeper/beepy/main/raspberrypi/disable_motd.sh" | bash
 curl -s "https://raw.githubusercontent.com/beeper/beepy/main/raspberrypi/disable_cursor.sh" | bash
 curl -s "https://raw.githubusercontent.com/beeper/beepy/main/raspberrypi/optimize_shutdown.sh" | bash
