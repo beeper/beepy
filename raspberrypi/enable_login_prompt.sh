95987c25551cb56a90a445a91320ef1d42d9e7c5
drewry pope <drewrypope@gmail.com>
2023-08-27 23:59:57 -0500

    del

--- raspberrypi/enable_login_prompt.sh
+++ raspberrypi/enable_login_prompt.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 modify_service_conf() {
   modify_service_conf_file="$1"
   modify_service_conf_linePrefix="$2"
@@ -14,23 +16,26 @@ modify_service_conf() {
   modify_service_conf_new_content=""
   modify_service_conf_found=0
 
-  while IFS= read -r modify_service_conf_line || [ -n "$modify_service_conf_line" ]; do
+  while IFS= read -r modify_service_conf_line || [ -n "${modify_service_conf_line}" ]; do
     if echo "${modify_service_conf_line}" | grep -q -e "^${modify_service_conf_linePrefix_escaped}"; then
       modify_service_conf_found=1
+      #shellcheck disable=2295
       remaining_line="${modify_service_conf_line#${modify_service_conf_linePrefix} }"
       case "${modify_service_conf_subcommand}" in
         add)
           if ! echo "${remaining_line}" | grep -q -w -e "${modify_service_conf_flag_escaped}"; then
-            modify_service_conf_line="${modify_service_conf_line} ${modify_service_conf_flag}"
+            modify_service_conf_line="${modify_service_conf_linePrefix} ${modify_service_conf_flag} ${remaining_line}"
           fi
           ;;
         del|delete)
           remaining_line=$(echo "${remaining_line}" | sed "s/[[:space:]]*${modify_service_conf_flag_escaped}[[:space:]]*/ /g" | awk '{$1=$1};1')
           modify_service_conf_line="${modify_service_conf_linePrefix} ${remaining_line}"
           ;;
+        *)
+          ;;
       esac
     fi
-    modify_service_conf_new_content="${modify_service_conf_new_content}${modify_service_conf_line}"$'\n'
+    modify_service_conf_new_content=$(printf "%s\n" "${modify_service_conf_new_content}${modify_service_conf_line}")
   done < "${modify_service_conf_file}"
 
   if [ "${modify_service_conf_found}" -eq 0 ]; then
