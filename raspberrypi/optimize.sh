c220946175d51e1df1d5b3f667c07380fb56dd04
drewry pope <drewrypope@gmail.com>
2023-08-27 19:00:18 -0500

    refactor

--- raspberrypi/optimize.sh
+++ raspberrypi/optimize.sh
@@ -2,5 +2,5 @@
 
 curl -s "https://raw.githubusercontent.com/dezren39-dev/beepy/optimize2/raspberrypi/optimize_boot.sh" | bash
 curl -s "https://raw.githubusercontent.com/dezren39-dev/beepy/optimize2/raspberrypi/optimize_motd.sh" | bash
-curl -s "https://raw.githubusercontent.com/dezren39-dev/beepy/optimize2/raspberrypi/optimize_power.sh" | bash
+curl -s "https://raw.githubusercontent.com/dezren39-dev/beepy/optimize2/raspberrypi/disable_cursor.sh" | bash
 curl -s "https://raw.githubusercontent.com/dezren39-dev/beepy/optimize2/raspberrypi/optimize_shutdown.sh" | bash
